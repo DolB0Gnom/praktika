a4b465f18379f29e76928752312e63b9a63f2fa0
DolB0Gnom <maks.kotlenko05@mail.ru>
2024-04-05 18:07:51 +0300

    массивы

---
+++ задачи по массивам/zadanie 1/zadanie 1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 4, 7, 12, 5, 2, 8, 9, 14, 3 };
+
+        Console.WriteLine("Нечетные числа:");
+        foreach (var num in array)
+        {
+            if (num % 2 != 0)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
---
+++ задачи по массивам/zadanie 2/zadanie 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] array = { "строка 1", "строка 2", "строка 3", "строка 4" };
+
+        Console.Write("Введите новую строку: ");
+        string userInput = Console.ReadLine();
+
+        bool isStringFound = false;
+        foreach (var str in array)
+        {
+            if (str == userInput)
+            {
+                isStringFound = true;
+                break;
+            }
+        }
+
+        if (isStringFound)
+        {
+            Console.WriteLine("Строка найдена!");
+        }
+        else
+        {
+            Console.WriteLine("Строка не найдена!");
+        }
+    }
+}
